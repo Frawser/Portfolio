50b8c6477bb765cd4daaf0e2cea3efd0ab9129ef
Frawser <mikis011@hotmail.com>
2024-04-18 15:16:38 +0200

    update

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   )
 }
 
